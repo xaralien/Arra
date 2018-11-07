d5632bd6a36085db04426d49d9dc7b5b0ebaf33c
Xaralien <30455825+xaralien@users.noreply.github.com>
2018-11-07 18:24:23 +0700

    Update index.js

--- index.js
+++ index.js
@@ -78,7 +78,7 @@ const commands = {
 		msg.channel.sendMessage(`__**${msg.guild.name}'s Music Queue:**__ Currently **${tosend.length}** songs queued ${(tosend.length > 15 ? '*[Only next 15 shown]*' : '')}\n\`\`\`${tosend.slice(0,15).join('\n')}\`\`\``);
 	},
 	'help': (msg) => {
-		let tosend = ['```xl',tokens.prefix + 'ciduk : "hmmm......"', tokens.prefix + 'angry : "Angry Command"', tokens.prefix + 'join : "Join Voice channel of msg sender"',	tokens.prefix + 'add : "Add a valid youtube link to the queue"', tokens.prefix + 'queue : "Shows the current queue, up to 15 songs shown."', tokens.prefix + 'play : "Play the music queue if already joined to a voice channel"', '', 'the following commands only function while the play command is running:'.toUpperCase(), tokens.prefix + 'pause : "pauses the music"',	tokens.prefix + 'resume : "resumes the music"', tokens.prefix + 'skip : "skips the playing song"', tokens.prefix + 'time : "Shows the playtime of the song."',	'volume+(+++) : "increases volume by 2%/+"',	'volume-(---) : "decreases volume by 2%/-"',	'```'];
+		let tosend = ['```xl',tokens.prefix + 'waiting : " Still Waiting!"',tokens.prefix + 'ferguso : " Not that easy Ferguso!"',tokens.prefix + 'ciduk : "hmmm......"', tokens.prefix + 'angry : "Angry Command"', tokens.prefix + 'join : "Join Voice channel of msg sender"',	tokens.prefix + 'add : "Add a valid youtube link to the queue"', tokens.prefix + 'queue : "Shows the current queue, up to 15 songs shown."', tokens.prefix + 'play : "Play the music queue if already joined to a voice channel"', '', 'the following commands only function while the play command is running:'.toUpperCase(), tokens.prefix + 'pause : "pauses the music"',	tokens.prefix + 'resume : "resumes the music"', tokens.prefix + 'skip : "skips the playing song"', tokens.prefix + 'time : "Shows the playtime of the song."',	'volume+(+++) : "increases volume by 2%/+"',	'volume-(---) : "decreases volume by 2%/-"',	'```'];
 		msg.channel.sendMessage(tosend.join('\n'));
 	},
 	'reboot': (msg) => {
@@ -103,6 +103,9 @@ const commands = {
 	},
 	'ferguso': (msg) => {
 		msg.channel.send("", {files: ["https://media.discordapp.net/attachments/440518356798537739/509630012425502771/45491625_327822608046422_7053969203648266240_n.jpg"]});
+	},
+	'waiting': (msg) => {
+		msg.channel.send("", {files: ["https://thumbs.gfycat.com/ImpracticalHarshBangeltiger-small.gif"]});
 	}
 };
 
