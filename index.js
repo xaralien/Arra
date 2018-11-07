fc2419de360e7f93f6b737fae8a21231722bf1ea
Xaralien <30455825+xaralien@users.noreply.github.com>
2018-11-07 17:47:48 +0700

    Update index.js

--- index.js
+++ index.js
@@ -108,6 +108,13 @@ const commands = {
 
 client.on('ready', () => {
 	console.log('ready!');
+
+	client.user.setStatus('Online')// Your status goes here; It can be 'Online','Idle','Invisible', & 'dnd'
+
+	// Game & Streaming
+	client.user.setGame('Hello') // You can change the string to whatever you want it to say.
+	// To set a stream, add another option like this:
+	btoa.user.setGame('Command : "arra " add space and ', 'https://www.youtube.com/watch?v=BRVxGBx6_9Q');
 });
 
 client.on('message', msg => {
